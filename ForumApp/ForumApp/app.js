ed6db97783c3513809f4c2a8400ceda7c5d5ce04
Denise <murrain.denise@gmail.com>
2018-01-25 15:01:23 -0500

    created css styling, closes #1

--- ForumApp/ForumApp/app.js
+++ ForumApp/ForumApp/app.js
@@ -4,15 +4,19 @@ var ejs = require('ejs');
 var app = express();
 app.set("view engine", "ejs");
 
+app.use('/public',express.static('public'));
+
 var server = app.listen(3000, listening);
 
 
 function listening() {
   console.log("listening");
-}
+};
 
 app.get("/", function (request, response) {
 
   console.log();("working");
+//    response.send("yo")
 
-})
+    response.render('index.ejs'); 
+});
